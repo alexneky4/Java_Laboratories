9de0da0bc9e4ad7bec78979a1bb5a08ef22b5e5e
NECHIFOR Alexandru <100467862+alexneky4@users.noreply.github.com>
2023-03-01 11:03:55 +0200

    added functions

--- Laboratory1/src/uaic/java/Laboratory1.java
+++ Laboratory1/src/uaic/java/Laboratory1.java
@@ -3,13 +3,10 @@ package uaic.java;
 import java.util.Scanner;
 
 public class Laboratory1 {
+	public static void mandatory()
+	{
+		System.out.println("Hello World!");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	//Mandatory	
-		/*
-		 * System.out.println("Hello World!");
-		
 		String languages[] = new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
 		int n = (int) (Math.random() * 1_000_000);
 		n = n * 3;
@@ -34,9 +31,11 @@ public class Laboratory1 {
 			finalResult = 0;
 		}
 		System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
-		*/
-		//Homework
-		/*long time = System.currentTimeMillis();
+
+	}
+	public static void homework(String[] args)
+	{
+		long time = System.currentTimeMillis();
 		if(args.length  != 1)
 		{
 			System.out.println("You have to give only a  number as argument in the command line!");
@@ -69,29 +68,24 @@ public class Laboratory1 {
 		{
 			for(int i = 0; i < n; i++)
 			{
-				StringBuilder line = new StringBuilder();
-				StringBuilder column = new StringBuilder();
 				for(int j = 0; j < n; j++)
 				{
-					line.append(matrix[i][j]);
-					column.append(matrix[j][i]);
+					System.out.print(matrix[i][j]);
 				}
-				System.out.println(line);
-				System.out.println(column);
+				System.out.println();
 			}
 		}
 		else
 		{
 			System.out.println(System.currentTimeMillis() - time);
 		}
-		*/
-		
-		//Bonus
-	/*	
-		//Matrix of a cyclic graph
+	}
+
+	public static void bonus1()
+	{
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		
+
 		int matrixA[][] = new int[n][n];
 		for(int i = 0; i < n; i ++)
 		{
@@ -111,7 +105,7 @@ public class Laboratory1 {
 				matrixA[i][i+1] = 1;
 			}
 		}
-		
+
 		int matrixPower[][] = new int[n][n];
 
 		for(int x = 1; x <= n; x++)
@@ -124,7 +118,7 @@ public class Laboratory1 {
 						matrixPower[i][j] = matrixA[i][j];
 					else
 					{
-						
+
 						aux[i][j] = 0;
 						for(int k = 0; k < n; k++)
 							aux[i][j] += matrixA[i][k] * matrixPower[k][j];
@@ -142,21 +136,21 @@ public class Laboratory1 {
 			}
 			System.out.println("-------");
 		}
-		*/
-		
-		//Matrix of a regular graph
-		
+	}
+
+	public static void bonus2(String[] args)
+	{
 		if(args.length != 2)
 		{
 			System.out.println("Give the number of vertices and the degree of the graph");
 			System.exit(-1);
 		}
-		
+
 		int n = Integer.parseInt(args[0]);
 		int k = Integer.parseInt(args[1]);
-		
+
 		int matrix[][] = new int[n][n];
-		
+
 		for(int i = 0; i < n; i++ )
 			for(int j = 0; j < n; j++)
 				matrix[i][j] = 0;
@@ -168,7 +162,7 @@ public class Laboratory1 {
 		}
 		for(int i = 0; i < n; i++)
 		{
-			
+
 			int ck = k;
 			int neighbour = k/2;
 			int left = i - neighbour;
@@ -177,7 +171,7 @@ public class Laboratory1 {
 				matrix[i][n + left] = 1;
 				left++;
 				ck--;
-				
+
 			}
 			if(ck == k)
 			{
@@ -187,21 +181,21 @@ public class Laboratory1 {
 					ck--;
 				}
 			}
-			
+
 			while(i + ck >= n)
 			{
 				matrix[i][i+ck - n] = 1;
 				ck--;
 			}
-			
+
 			while(ck > 0)
 			{
 				matrix[i][i + ck] = 1;
 				ck--;
 			}
-			
+
 		}
-		
+
 		for(int i = 0; i < n; i++)
 		{
 			for(int j = 0; j < n; j++)
@@ -210,6 +204,12 @@ public class Laboratory1 {
 			}
 			System.out.print('\n');
 		}
-	}
 
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//mandatory();
+		//homework(args);
+		bonus1();
+	}
 }
