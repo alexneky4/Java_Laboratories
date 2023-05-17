62c1e13ee55793a2b435077f2276cd9361d41886
Nechifor Alexandru <100467862+alexneky4@users.noreply.github.com>
2023-05-17 08:38:22 +0300

    Lab 11

---
+++ Lab11/src/test/java/uaic/info/Lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package uaic.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
